be4e251e8b4cadf6ef0b955c0d95bb1bbf53c647
Jason Gordon <jason.m.gordon@gmail.com>
2021-01-08 20:01:15 -0800

    fix: base 64 decoding during server side rendering

--- src/helpers/index.jsx
+++ src/helpers/index.jsx
@@ -1,5 +1,6 @@
 export const isBrowser = () => typeof window !== "undefined";
 
-export const base64Decode = (str) => (isBrowser() ? atob(str) : (str) => str);
+export const base64Decode = (str) =>
+  isBrowser() ? atob(str) : Buffer.from(str, "base64").toString("utf-8");
 
 export const buildPageTitle = (title) => `Jason Gordon - ${title}`;
