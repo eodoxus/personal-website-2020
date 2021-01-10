2dcf6a7b80b496c19cfa560ae8738a16d9ea65d9
Jason Gordon <jason.m.gordon@gmail.com>
2021-01-10 12:19:26 -0800

    fix: page slice height on mobile

--- src/containers/Page/Page.jsx
+++ src/containers/Page/Page.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Head, useRouteData } from "react-static";
 
 import SliceRenderer from "../../components/slices/SliceRenderer";
@@ -8,17 +8,29 @@ import { buildPageTitle } from "../../helpers";
 import styles from "./Page.module.scss";
 
 export default () => {
+  const ref = useRef();
   const { page } = useRouteData();
   const { slices, title } = page;
+
+  const [viewportHeight, setViewportHeight] = useState(0);
+  useEffect(() => {
+    setViewportHeight(
+      ref.current.parentNode.parentNode.getBoundingClientRect().height
+    );
+  }, []);
 
   return (
     <SlideInOverlay className={styles.viewport}>
       <Head>
         <title>{buildPageTitle(title)}</title>
       </Head>
-      <div className={styles.viewport}>
+      <div className={styles.viewport} ref={ref}>
         {slices.map((slice, idx) => (
-          <div className={styles.slice} key={idx}>
+          <div
+            className={styles.slice}
+            key={idx}
+            style={{ height: viewportHeight }}
+          >
             <SliceRenderer slice={slice} />
           </div>
         ))}
